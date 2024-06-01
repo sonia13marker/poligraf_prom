b8937f9f05439fbc125802a8ce86bf1481e2cd52
SoNya Marker <soniamarker13@gmail.com>
2024-06-01 22:19:04 +0300

    first slider in main page without logic

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,10 +1,12 @@
 import HeaderMainBlock from "../../components/HeaderMainBlock/HeaderMainBlock";
 import AboutCompanyComponent from "../../components/AboutCompanyComponent/AboutCompanyComponent";
+import AboutCompanySlider from "../../components/AboutCompanySlider/AboutCompanySlider";
 
 
 export default function MainPage() {
 	return <div>
 		<HeaderMainBlock/>
 		<AboutCompanyComponent />
+		<AboutCompanySlider />
 	</div>
 }
