bb4d92a2414c8267f06933c1dd75f2dcf35f7a1b
SoNya Marker <soniamarker13@gmail.com>
2024-06-06 17:28:32 +0300

    start providers slider logic

--- src/components/ProvidersSlider/ProvidersSlider.jsx
+++ src/components/ProvidersSlider/ProvidersSlider.jsx
@@ -1,44 +1,60 @@
 import styles from "./ProvidersSlider.module.scss";
-import logo1 from "../../images/pr-logo-1.png";
-import logo2 from "../../images/pr-logo-2.png";
-import logo3 from "../../images/pr-logo-3.png";
-import logo4 from "../../images/pr-logo-4.png";
-import ProvidersSliderCard from "../ProvidersSliderCard/ProvidersSliderCard";
 import { Link } from "react-router-dom";
 import ButtonRightSliderBorder from "../../icons/ButtonRightSliderBorder";
 import ButtonLeftSliderBorder from "../../icons/ButtonLeftSliderBorder";
+import providers from "../../data/providers.json";
+import { useState } from "react";
+import ProvidersSliderCard from "../ProvidersSliderCard/ProvidersSliderCard";
 
 export default function ProvidersSlider() {
-  const providers = [
-    { name: "Inknovators", image: `${logo1}` },
-    { name: "Amagic", image: `${logo2}` },
-    { name: "Colors", image: `${logo3}` },
-    { name: "Creative", image: `${logo4}` },
-  ];
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const nextSlide = () => {
+    setCurrentImageIndex((currentImageIndex + 2) % providers.providers.length);
+  };
+
+  const prevSlide = () => {
+    setCurrentImageIndex(
+      (currentImageIndex - 2 + providers.providers.length) %
+        providers.providers.length,
+    );
+  };
+
   return (
     <span className={styles.wrapper}>
       <span className={styles.wrapper__header}>
         <h2 className={styles.wrapper__header__title}>Поставщики</h2>
         {/*место для кнопок переключения слайдера*/}
+        <span className={styles.wrapper__header__buttons}>
+          <ButtonLeftSliderBorder
+            type="providers"
+            onClickAction={nextSlide}
+            color={currentImageIndex === 0 ? "#D5D6D7" : "#007CBC"}
+          />
+          <ButtonRightSliderBorder
+            type="providers"
+            onClickAction={prevSlide}
+            color={currentImageIndex === 7 ? "#D5D6D7" : "#007CBC"}
+          />
+        </span>
       </span>
 
       <div className={styles.wrapper__slider}>
-        {providers.map((provider, i) => (
-          <ProvidersSliderCard
-            index={i}
-            name={provider.name}
-            image={provider.image}
-            type="slider"
-          />
-        ))}
+        {providers.providers
+          .slice(currentImageIndex, currentImageIndex + 4)
+          .map((provider, i) => (
+            // console.log(provider),
+            <ProvidersSliderCard
+              key={i}
+              name={provider.name}
+              image={provider.image}
+              type="slider"
+            />
+          ))}
       </div>
 
       <Link to={"/providers"} className={styles.wrapper__button}>
         Посмотреть еще
       </Link>
-
-      <ButtonLeftSliderBorder type="providers" />
-      <ButtonRightSliderBorder type="providers" />
     </span>
   );
 }
