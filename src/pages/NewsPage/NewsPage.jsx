80353c405bf2affe877a69d9843ca3e7cc8b00fc
SoNya Marker <soniamarker13@gmail.com>
2024-06-10 22:02:23 +0300

    NewsPage adaptation

--- src/pages/NewsPage/NewsPage.jsx
+++ src/pages/NewsPage/NewsPage.jsx
@@ -1,9 +1,10 @@
 import HeaderForPages from "../../components/HeaderForPages/HeaderForPages";
 import NewsComponent from "../../components/NewsComponent/NewsComponent";
+import style from "./NewsPage.module.scss";
 
 export default function NewsPage() {
   return (
-    <div>
+    <div className={style.newsPage}>
       <HeaderForPages title="Новости" searchMock="Найти новость..." />
       <NewsComponent />
     </div>
