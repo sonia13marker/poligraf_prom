1f084fa3c8e80ee8e002c688ad22a6c4624b93d5
SoNya Marker <soniamarker13@gmail.com>
2024-06-11 20:40:12 +0300

    scroll to top on pages component

--- src/components/Wrapper/Wrapper.jsx
+++ src/components/Wrapper/Wrapper.jsx
@@ -4,10 +4,14 @@ import { Outlet } from "react-router-dom";
 import style from "./Wrapper.module.scss";
 import HeaderMobile from "../HeaderMobile/HeaderMobile";
 import ResizeComponent from "../ResizeComponent/ResizeComponent";
+import ScrollToTop from "../ScrollToTop";
 
 export default function Wrapper() {
   return (
     <div className={style.wrapper}>
+      {/*scroll to top on pages*/}
+      <ScrollToTop />
+      {/*--*/}
       <ResizeComponent
         resizeValue="1015"
         defaultComponent={<Header />}
