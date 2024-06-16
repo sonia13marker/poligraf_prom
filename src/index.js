4853521f082fa899f24645afeb1d237267d4e66a
SoNya Marker <soniamarker13@gmail.com>
2024-06-16 18:26:34 +0300

    change router

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/poligraf_prom">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
