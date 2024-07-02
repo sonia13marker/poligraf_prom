7490547076c44d133472a42bd2596e2b3b27bd03
SoNya Marker <soniamarker13@gmail.com>
2024-07-02 18:31:00 +0300

    AdFirst - new changes

--- src/components/ADFirst/ADFirst.jsx
+++ src/components/ADFirst/ADFirst.jsx
@@ -1,5 +1,7 @@
 import styles from "./ADFirst.module.scss";
 import img from "../../images/AD-1.png";
+import img1 from "../../images/AD-1-small.png";
+import ResizeComponent from "../ResizeComponent/ResizeComponent";
 
 export default function ADFirst() {
   return (
@@ -8,13 +10,30 @@ export default function ADFirst() {
         <h3 className={styles.background__text__title}>
           Оказываем <br /> поддержку 24/7
         </h3>
+        <ResizeComponent
+          defaultComponent={<></>}
+          resizedComponent={
+            <span className={styles.background__text__number}>
+              +7-918-076-14-64
+            </span>
+          }
+          resizeValue="925"
+        />
         <p className={styles.background__text__p}>
           Поможем разобраться в деталях, объясним принцип работы, правильно
           настроим оборудование, проконсультируем перед покупкой и всегда будем
           на связи после приобретения товара
         </p>
       </span>
-      <img src={img} alt="" className={styles.background__image} />
+      <ResizeComponent
+        defaultComponent={
+          <img src={img} alt="" className={styles.background__image} />
+        }
+        resizedComponent={
+          <img src={img1} alt="" className={styles.background__image} />
+        }
+        resizeValue="925"
+      />
     </span>
   );
 }
