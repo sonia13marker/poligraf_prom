0a0ea930d1dfaaddbd2bf370a5ecee7cd768e7b6
SoNya Marker <soniamarker13@gmail.com>
2024-07-09 17:06:12 +0300

    new changes in block about company

--- src/components/AboutCompanyComponent/AboutCompanyComponent.jsx
+++ src/components/AboutCompanyComponent/AboutCompanyComponent.jsx
@@ -27,32 +27,28 @@ export default function AboutCompanyComponent() {
         </span>
         <span className={styles.about__wrapper__twoHalf}>
           <span className={styles.about__wrapper__twoHalf__wrapper}>
-            {/*<span className={styles.about__wrapper__twoHalf__wrapper__one}>*/}
-            <AboutCompanySmallBlock
-              text="Стабильность - наш ключ к успеху, мы работаем уже более 13 лет"
-              color={`$catalog-page-color`}
-            />
-            <AboutCompanySmallBlock
-              text="Качественная продукция, подтвержденная сертификатами ISO"
-              color={`$contacts-page-color`}
-            />
-            <AboutCompanySmallBlock
-              text="Мы предоставляем наши услуги клиентам всего Южного федерального округа"
-              color={`$catalog-page-color`}
-            />
-            {/*</span>*/}
-            <AboutCompanySmallBlock
-              text="Бесплатная доставка по городу при заказе от 5 тысяч рублей"
-              color={`$catalog-page-color`}
-              type="accent"
-            />
+            <span className={styles.about__wrapper__twoHalf__wrapper__one}>
+              <AboutCompanySmallBlock
+                text="Стабильность - наш ключ к успеху, мы работаем уже более 13 лет"
+                color={`$catalog-page-color`}
+              />
+              <AboutCompanySmallBlock
+                text="Качественная продукция, подтвержденная сертификатами ISO"
+                color={`$contacts-page-color`}
+              />
+            </span>
 
-            {/*<span className={styles.about__wrapper__twoHalf__wrapper__two}>*/}
-            {/*<AboutCompanyLargeTopBlock*/}
-            {/*  color={`$news-page-color`}*/}
-            {/*  text="Лаборатория, где работает команда технологов и колористов"*/}
-            {/*/>*/}
-            {/*</span>*/}
+            <span className={styles.about__wrapper__twoHalf__wrapper__two}>
+              <AboutCompanySmallBlock
+                text="Мы предоставляем наши услуги клиентам всего Южного федерального округа"
+                color={`$catalog-page-color`}
+              />
+              <AboutCompanySmallBlock
+                text="Бесплатная доставка по городу при заказе от 5 тысяч рублей"
+                color={`$catalog-page-color`}
+                type="accent"
+              />
+            </span>
           </span>
         </span>
       </span>
