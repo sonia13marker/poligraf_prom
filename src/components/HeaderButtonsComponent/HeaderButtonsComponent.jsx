8f07f61565acb7e94634bac035fbadc5ad8aee14
SoNya Marker <soniamarker13@gmail.com>
2025-05-19 23:38:25 +0300

    animation for images in main block

--- src/components/HeaderButtonsComponent/HeaderButtonsComponent.jsx
+++ src/components/HeaderButtonsComponent/HeaderButtonsComponent.jsx
@@ -1,24 +1,25 @@
 import { Link } from "react-router-dom";
 import style from "./HeaderButtonsComponent.module.scss";
 
-export default function HeaderButtonsComponent({ links }) {
+export default function HeaderButtonsComponent({ links, onHover, onLeave }) {
   return (
     <>
-      {links.map((link, i) =>
-          <span key={i}>
-            <Link
-              to={link.linkTo}
-              className={
-                i === 0
-                  ? `${style.headerButtons}`
-                  : style.headerButtons
-              }
-            >
-              {link.title}
-            </Link>
-          </span>
-      )
-      }
+      {links.map((link, i) => (
+        <span key={i}>
+          <Link
+            to={link.linkTo}
+            className={
+              i === 0
+                ? `${style.headerButtons}`
+                : style.headerButtons
+            }
+            onMouseEnter={() => onHover(link.image)}
+            onMouseLeave={onLeave}
+          >
+            {link.title}
+          </Link>
+        </span>
+      ))}
     </>
   );
 }
--- src/components/HeaderMainBlock/HeaderMainBlock.jsx
+++ src/components/HeaderMainBlock/HeaderMainBlock.jsx
@@ -1,22 +1,47 @@
 import styles from "./HeaderMainBlock.module.scss";
 import HeaderButtonsComponent from "../HeaderButtonsComponent/HeaderButtonsComponent";
+import { useState } from "react";
+import pic1 from "../../images/main_block_1.png";
+import pic2 from "../../images/main_block_2.png";
+import pic3 from "../../images/main_block_3.png";
 
 export default function HeaderMainBlock() {
+  const [activeImage, setActiveImage] = useState(`${pic1}`); // Картинка по умолчанию
   const links = [
-    { linkTo: "/catalog", title: "Всё для графической печати", type: "scroll" },
-    { linkTo: "/catalog", title: "Всё для текстильной печати" },
-    { linkTo: "/catalog", title: "Всё для ТПФ" },
+    { 
+      linkTo: "/catalog", 
+      title: "Всё для графической печати", 
+      type: "scroll",
+      image: `${pic1}` // Картинка для первой кнопки
+    },
+    { 
+      linkTo: "/catalog", 
+      title: "Всё для текстильной печати",
+      image: `${pic2}` // Картинка для второй кнопки
+    },
+    { 
+      linkTo: "/catalog", 
+      title: "Всё для ТПФ",
+      image: `${pic3}` // Картинка для третьей кнопки
+    },
   ];
 
   return (
     <div className={styles.headerWrapper}>
       <h1 className={styles.headerWrapper__title}>
-      Решения для вашего шёлкотрафаретного бизнеса
+        Решения для вашего шёлкотрафаретного бизнеса
       </h1>
-      <span className={styles.headerWrapper__buttons}>
-          <HeaderButtonsComponent links={links} />
-        </span>
-      <div className={styles.headerWrapper__imageBlock}>
+      <div className={styles.headerWrapper__buttons}>
+        <HeaderButtonsComponent 
+          links={links} 
+          onHover={(image) => setActiveImage(image)}
+          onLeave={() => setActiveImage(links[0].image)} // Возвращаем первую картинку при уходе
+        />
+      </div>
+      <div 
+        className={styles.headerWrapper__imageBlock}
+        style={{ backgroundImage: `url(${activeImage})` }}
+      >
       </div>
     </div>
   );
